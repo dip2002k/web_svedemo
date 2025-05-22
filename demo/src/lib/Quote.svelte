e860377a2a15647c853d797a700a7962deb49606
dip2002k <keva2@student.op.ac.nz>
2025-05-22 15:22:18 +1200

    quote file

--- demo/src/lib/Quote.svelte
+++ demo/src/lib/Quote.svelte
@@ -1,9 +1,13 @@
 <script>
-
 </script>
 
+<div class="Quote-container">
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, ad
+    blanditiis unde voluptate exercitationem, magnam corporis minus quae ex ut
+    placeat eaque similique hic velit tenetur fugit facilis nihil deleniti!
+  </p>
+</div>
 
 <style>
-
-    
 </style>
